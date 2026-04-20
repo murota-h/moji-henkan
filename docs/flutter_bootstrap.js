41bed989513d956f7981d7a735689a6741088ab4
murota-h <renakaren0804@gmail.com>
2026-04-20 17:41:56 +0900

    アイコン・アプリ名をもじもじチェンジャーに更新

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2195344621" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3012555757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
