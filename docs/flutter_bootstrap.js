4b3ecf2268745b0fe6c1178ecad55f8cd9f8ea56
murota-h <renakaren0804@gmail.com>
2026-04-20 18:01:22 +0900

    iOS Safari テキスト入力対応

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "852026992" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3294998112" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
